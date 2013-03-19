d5e820646c4a04138fe6ed88358c143b249b3adc
Brandon Ewoldt <bewoldt1@gmail.com>
2013-03-19 16:31:36 -0500

    working on division bar

--- html/inc/division-topbar.php
+++ html/inc/division-topbar.php
@@ -1,21 +1,15 @@
 <div class="division-topbar">
     <div class="container clearfix">
-        <h1 class="univ-heading">
-            <a href="http://www.uiowa.edu/">The University of Iowa</a>
-            <span><a href="http://studentlife.uiowa.edu/">Division of Student Life</a></span>
-        </h1>
-        <button type="button" class="menu-link">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <nav role="navigation">
-            <ul class="univ-nav" role="navigation">
-                <li><a href="http://imu.uiowa.edu">Iowa Memorial Union</a></li>
-                <li><a href="http://recserv.uiowa.edu/">Recreational Services</a></li>
-                <li><a href="http://imu.uiowa.edu/students/">Student Involvement</a></li>
-                <li><a href="http://studentlife.uiowa.edu/">All Departments</a></li>
-            </ul>
-        </nav>
+        <a href="http://www.uiowa.edu/" class="uiowa">
+            <img src="images/uiowa.png" alt="The University of Iowa" width="177">
+        </a>
+        <form class="division-search">
+            <label for="search" class="visuallyhidden">Search this site</label>
+            <input type="text" id="search" placeholder="Search this site" class="division-search-input">
+            <input type="submit" value="Search" class="division-search-btn">
+        </form>
+        <a href="#" class="division-studentlife">
+            <img src="images/division_studentlife.png" alt="Division of Student Life" width="240">
+        </a>
     </div>
 </div>
