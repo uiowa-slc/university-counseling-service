fa802df9b1549830bf89de2ea3ed4be643e1a86f
brandon ewoldt <brandonewoldt@brandon-ewoldts-iMac.local>
2013-04-04 07:35:08 -0500

    interior page created

--- html/interior.php
+++ html/interior.php
@@ -13,110 +13,77 @@
 
     <?php include 'inc/header.php'; ?>
 
-    <div class="hero">
-        <div class="container">
-            <div class="hero-text">
-                <h2 class="blocktext">We offer a variety of counseling services, outreach, and training for U.of Iowa students and staff.</h2>
-                <ul>
-                    <li><a href="#">Counseling</a></li>
-                    <li><a href="#">Outreach</a></li>
-                    <li><a href="#">Training</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-
-    <section class="home-highlights">
-        <div class="container clearfix">
-            <div class="module">
-                <div class="media">
-                    <a href="#">
-                        <img src="images/home_staff.jpg" alt="Our Staff">
-                    </a>
-                </div>
-                <div class="inner">
-                    <h3><a href="#">Our Staff</a></h3>
-                    <p>We are a professional staff of twelve doctoral-level psychologists, one case manager, and three psychology interns. <a href="#">View our staff profiles.</a></p>
-                </div>
-            </div>
-            <div class="module">
-                <div class="media">
-                    <a href="#">
-                        <img src="images/home_outreach.jpg" alt="Our Staff">
-                    </a>
-                </div>
-                <div class="inner">
-                    <h3><a href="#">Outreach Programs</a></h3>
-                    <p>Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus. Cras justo odio dapibus facilisis in egestas eget.</p>
-                </div>
-            </div>
-            <div class="module">
-                <div class="media">
-                    <a href="#">
-                        <img src="images/home_diversity.jpg" alt="Our Staff">
-                    </a>
-                </div>
-                <div class="inner">
-                    <h3><a href="#">We Welcome Diversity</a></h3>
-                    <p>In all individual, group, and program services we strive to create an environment where all people feel welcome. Learn more.</p>
-                </div>
-            </div>
-        </div><!-- end .container -->
-    </section>
-
-    <section class="main" role="main">
-    <div class="container">
+    <div class="main-bg">
         
-            <h2>Index</h2>
-
-            <p>Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam quis risus eget urna mollis ornare vel eu leo. Donec sed odio dui.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae velit quo blanditiis alias aliquam sequi dolores non tempore illo enim accusantium optio quisquam commodi fugit dolorum ad consequatur rerum quos.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque quae blanditiis deserunt necessitatibus temporibus rerum velit optio consectetur in fugit minima deleniti sed laudantium dolores architecto dicta id harum molestias neque vero nulla tenetur voluptatibus sunt voluptate beatae dignissimos cum laborum laboriosam amet. At minima esse sint aspernatur veritatis mollitia. </p>
-            <h3>Lorem ipsum dolor sit.</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur dolorem quibusdam aspernatur et commodi ratione officiis numquam ipsa ad vero quasi sint nulla tempora illo asperiores deleniti sit delectus error totam quisquam veritatis nostrum iste ullam facilis aperiam praesentium eos dicta. Odio ab accusantium totam neque voluptatum dolorem aliquid itaque!</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti dicta est voluptates commodi molestias adipisci rerum in inventore non eum velit id illo ipsum saepe veritatis consequatur esse itaque aperiam!</p>
-        </div>
-    </section>
-    <section>
-        <div class="container">
+        <section class="container content-wrapper">
+            <ul class="breadcrumbs">
+                <li><a href="#">Home</a> <span class="divider">/</span></li>
+                <li class="active">Self Help</li>
+            </ul>
             <div class="row-fluid">
-                <div class="span6">
-                    <h3>Self-Help Topics</h3>
-                    <ul class="two-col">
-                        <li>Lorem ipsum dolor.</li>
-                        <li>Itaque recusandae tempore.</li>
-                        <li>Neque rem maxime.</li>
-                        <li>Error quos amet!</li>
-                        <li>Rem et nam!</li>
-                        <li>Quos nam reprehenderit.</li>
-                        <li>Nam animi aliquid!</li>
-                        <li>Vitae repellendus distinctio.</li>
-                    </ul>
-                </div>
-                <div class="span3">
-                    <h3>Latest News</h3>
-                    <ul>
-                        <li>
-                            <a href="#">Eating Disorder Peer Education Program for Student Volunteers</a>
-                            <small>Feb. 26</small>
-                        </li>
-                        <li>
-                            <a href="#">Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</a>
-                            <small>Feb. 13</small>
-                        </li>
-                    </ul>
-                </div>
-                <div class="span3">
-                    <h3>Upcoming Events</h3>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
+                <section class="span9 main-content">
+                    <h1>How to Prepare For Exams</h1>
+                    <p>The purpose of the written examination is to provide the instructor with evidence regarding the amount of knowledge the student has acquired in the course. It is not feasible to ask questions about everything in the course; therefore, the instructor tries to obtain a broad sample of the student's knowledge. Before making the test, the instructor evaluates the content of the course in terms of the importance of the materials and then phrases the questions in such a way as to reveal the facility of the student in using the concepts and ideas considered basic to the course.</p>
+                    <p>The student's ability to meet the requirements of the instructor depends primarily upon the effectiveness of his/her study methods during the term. If his/her methods have been poor, then the task of preparing for a final examination becomes extremely arduous with chances of success correspondingly reduced.</p>
+
+                </section>
+                <section class="span3 secondary-content">
+                    <nav>
+                        <h3>Self Help</h3>
+                        <ul>
+                            <li><a href="#">Lorem ipsum dolor.</a></li>
+                            <li><a href="#">Aliquam, dolore, consequatur.</a></li>
+                            <li><a href="#">Esse, iure, eius.</a></li>
+                            <li><a href="#">Quos, ex, odit!</a></li>
+                            <li><a href="#">Sequi, saepe, assumenda.</a></li>
+                        </ul>
+                    </nav>
+                </section>
+            </div>
+        </section>
+
+        <section class="padding">
+            <div class="container">
+                <div class="row-fluid">
+                    <div class="span6 mod">
+                        <h3 class="mod-title">Self-Help Topics</h3>
+                        <ul class="unstyled two-col">
+                            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            <li><a href="#">Laborum distinctio magni dolore recusandae?</a></li>
+                            <li><a href="#">Perferendis distinctio enim nisi magnam!</a></li>
+                            <li><a href="#">Aut perferendis nesciunt molestias in.</a></li>
+                            <li><a href="#">Nisi sunt vitae officia at.</a></li>
+                            <li><a href="#">Nihil provident eos quos earum.</a></li>
+                            <li><a href="#">Dolorum autem ab similique quaerat!</a></li>
+                            <li><a href="#">Quibusdam totam ad maxime repellat.</a></li>
+                        </ul>
+                    </div>
+                    <div class="span3 mod mod-news">
+                        <h3 class="mod-title">Latest News</h3>
+                        <ul class="unstyled">
+                            <li>
+                                <a href="#">Eating Disorder Peer Education Program for Student Volunteers</a>
+                                <small>Feb. 26</small>
+                            </li>
+                            <li>
+                                <a href="#">Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</a>
+                                <small>Feb. 13</small>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="span3 mod">
+                        <h3 class="mod-title">Upcoming Events</h3>
+                        <ul class="unstyled">
+                            <li><a href="#">Big 10 Counseling Centers Conference 2013</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet, consectetur</a></li>
+                            <li><a href="#">Lorem ipsum dolor sit amet</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+
+    </div>
 
     <?php include 'inc/footer.php'; ?>
 
