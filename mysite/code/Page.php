a3c2e3af14c27fbf162aadb4822519589aff41ef
Dustin Quam <quamsta@gmail.com>
2013-05-22 09:00:24 -0500

    blocking division bar css for manual entry on ucs, sls, csil

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -7,25 +7,25 @@ class Page extends SiteTree {
 
 	public static $has_one = array(
 	);
-	
-	
+
+
 	public static $many_many = array (
 		"SidebarItems" => "SidebarItem"
 	);
-	
+
     public static $many_many_extraFields=array(
         'SidebarItems'=>array(
             'SortOrder'=>'Int'
         )
     );
-    
+
     public static $plural_name = "Pages";
-	
+
 	public static $defaults = array (
-	
+
 		"InheritSidebarItems" => "1",
-	
-		"Content" => 
+
+		"Content" =>
 			"<h1>H1. This is a very large header.</h1>
 <p>The first paragraph directly after an H1 is the lede paragraph and is styled with a larger font size than other paragraphs.</p>
 <h2>H2. This is a large header.</h2>
@@ -38,82 +38,82 @@ class Page extends SiteTree {
 <p>Cum sociis natoque penatibus magnis parturient montes, nascetur ridiculus mus. Sed consectetur est.</p>
 <h6>H6. This is very small header.</h6>
 <p>Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor.</p>"
-	
+
 	);
-	
-	
+
+
 	public function getCMSFields(){
 		$f = parent::getCMSFields();
-		
+
 		$gridFieldConfig = GridFieldConfig_RelationEditor::create();
 		$gridFieldConfig->addComponent(new GridFieldManyRelationHandler(), 'GridFieldPaginator');
 
 		$gridField = new GridField("SidebarItems", "Sidebar Items", $this->SidebarItems(), $gridFieldConfig);
-		
-		
+
+
 		$gridFieldConfig2 = GridFieldConfig_RelationEditor::create();
 		$gridFieldConfig2->addComponent(new GridFieldSortableRows('SortOrder'));
-		
+
 		$gridField2 = new GridField("CurrentSidebarItems", "Sidebar Items", $this->SidebarItems(), $gridFieldConfig2);
-		
+
 		$f->addFieldToTab("Root.Sidebar", new LabelField("SidebarLabel", "<h2>Add sidebar items below</h2>"));
-		
+
 	/*	if($this->getParent()){
 		$f->addFieldToTab("Root.Sidebar", new CheckboxField("InheritSidebarItems", "Inherit parent page's sidebar items"));
 		}*/
-		$f->addFieldToTab("Root.Sidebar", $gridField); // add the grid field to a tab in the CMS	
-		
+		$f->addFieldToTab("Root.Sidebar", $gridField); // add the grid field to a tab in the CMS
+
 		$f->addFieldToTab("Root.Sidebar", new LabelField("SidebarLabel", "<h2>Sort the Sidebar Items Below</h2>"));
 
-		//$f->addFieldToTab("Root.Sidebar", $gridField2); // add the grid field to a tab in the CMS	
+		//$f->addFieldToTab("Root.Sidebar", $gridField2); // add the grid field to a tab in the CMS
 
 		return $f;
 	}
-	
-	
+
+
     public function SidebarItems() {
         return $this->getManyManyComponents('SidebarItems')->sort('SortOrder');
     }
 	/*public function inheritedSidebarItems(){
-	
+
 		$items = array();
-		
+
 		if($this->getParent()){
 
 			$parent = $this->getParent();
 			$parent_items = $parent->SidebarItems();
-			
+
 			foreach($parent_items as $parent_item){
 				$items[] = $parent_item;
 			}
 
-		}else{		
+		}else{
 			//echo $this->Title." is parentless";
-			$items = $this->SidebarItems()->toArray();	
-				
+			$items = $this->SidebarItems()->toArray();
+
 		}
-		
+
 		$itemList = new ArrayList($items);
-		
+
 		return $itemList;
-		
+
 	}*/
-	
+
 	/*public function getAllSidebarItems(){
-	
+
 		$items = array();
-		
+
 		$items[] = $this->SidebarItems()->toArray();
 		if(!$this->getParent()){
-		
+
 			return $items;
-			
+
 		}else if($this->InheritSidebarItems){
-		
+
 					}
-			
+
 		return $items;
-		
+
 	}*/
 
 }
@@ -140,22 +140,24 @@ class Page_Controller extends ContentController {
 	public function init() {
 		parent::init();
 
-		// Note: you should use SS template require tags inside your templates 
-		// instead of putting Requirements calls here.  However these are 
+		// Note: you should use SS template require tags inside your templates
+		// instead of putting Requirements calls here.  However these are
 		// included so that our older themes still work
 		Requirements::themedCSS('reset');
-		Requirements::themedCSS('layout'); 
-		Requirements::themedCSS('typography'); 
-		Requirements::themedCSS('form'); 
+		Requirements::themedCSS('layout');
+		Requirements::themedCSS('typography');
+		Requirements::themedCSS('form');
+
+    Requirements::block('division-bar/css/_division-bar.css');
 	}
-	
+
 	/* Clear Out Empty Lines from SS Template Indents */
-	/*public function handleRequest(SS_HTTPRequest $request, DataModel $model) { 
-		$ret = parent::handleRequest($request, $model); 
-		$temp=$ret->getBody(); 
-		$temp = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $temp); 
-		$ret->setBody($temp); 
-		return $ret; 
+	/*public function handleRequest(SS_HTTPRequest $request, DataModel $model) {
+		$ret = parent::handleRequest($request, $model);
+		$temp=$ret->getBody();
+		$temp = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $temp);
+		$ret->setBody($temp);
+		return $ret;
 	} */
 
 }
