4b8abea5b7dd1589e84a250f0083a2cf50ed6503
Dustin Quam <quamsta@gmail.com>
2013-11-21 11:24:52 -0600

    more things to convert to a division project

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -2,69 +2,11 @@
 class Page extends SiteTree {
 
 	private static $db = array(
-		
 	);
 
 	private static $has_one = array(
 	);
 
-
-	private static $many_many = array (
-		"SidebarItems" => "SidebarItem"
-	);
-
-    private static $many_many_extraFields=array(
-        'SidebarItems'=>array(
-            'SortOrder'=>'Int'
-        )
-    );
-
-    private static $plural_name = "Pages";
-
-	private static $defaults = array (
-
-
-		"Content" =>
-			"<h1>H1. This is a very large header.</h1>
-<p>The first paragraph directly after an H1 is the lede paragraph and is styled with a larger font size than other paragraphs.</p>
-<h2>H2. This is a large header.</h2>
-<p>Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient.</p>
-<h3>H3. This is a medium header.</h3>
-<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh ut fermentum massa justo.</p>
-<h4>H4. This is a moderate header.</h4>
-<p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl.</p>
-<h5>H5. This is small header.</h5>
-<p>Cum sociis natoque penatibus magnis parturient montes, nascetur ridiculus mus. Sed consectetur est.</p>
-<h6>H6. This is very small header.</h6>
-<p>Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor.</p>"
-
-	);
-
-
-	public function getCMSFields(){
-		$f = parent::getCMSFields();
-
-		$gridFieldConfig = GridFieldConfig_RelationEditor::create();
-		$gridFieldConfig->addComponent(new GridFieldManyRelationHandler(), 'GridFieldPaginator');
-
-		$gridField = new GridField("SidebarItems", "Sidebar Items", $this->SidebarItems(), $gridFieldConfig);
-		$f->addFieldToTab("Root.Sidebar", new LabelField("SidebarLabel", "<h2>Add sidebar items below</h2>"));
-		$f->addFieldToTab("Root.Sidebar", $gridField); // add the grid field to a tab in the CMS
-
-		/*$gridFieldConfig2 = GridFieldConfig_RelationEditor::create();
-		$gridFieldConfig2->addComponent(new GridFieldSortableRows('SortOrder'));
-		$gridField2 = new GridField("CurrentSidebarItems", "Sidebar Items", $this->SidebarItems(), $gridFieldConfig2);
-		$f->addFieldToTab("Root.Sidebar", new LabelField("SidebarLabel", "<h2>Sort the Sidebar Items Below</h2>"));
-		$f->addFieldToTab("Root.Sidebar", $gridField2); // add the grid field to a tab in the CMS*/
-
-		return $f;
-	}
-
-
-    public function SidebarItems() {
-        return $this->getManyManyComponents('SidebarItems')->sort('SortOrder');
-    }
-	
 }
 class Page_Controller extends ContentController {
 
@@ -89,24 +31,6 @@ class Page_Controller extends ContentController {
 	public function init() {
 		parent::init();
 
-		// Note: you should use SS template require tags inside your templates
-		// instead of putting Requirements calls here.  However these are
-		// included so that our older themes still work
-		Requirements::themedCSS('reset');
-		Requirements::themedCSS('layout');
-		Requirements::themedCSS('typography');
-		Requirements::themedCSS('form');
-
-    Requirements::block('division-bar/css/_division-bar.css');
 	}
-
-	/* Clear Out Empty Lines from SS Template Indents */
-	/*public function handleRequest(SS_HTTPRequest $request, DataModel $model) {
-		$ret = parent::handleRequest($request, $model);
-		$temp=$ret->getBody();
-		$temp = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $temp);
-		$ret->setBody($temp);
-		return $ret;
-	} */
 
 }
--- themes/ucs/templates/Page.ss
+++ themes/ucs/templates/Page.ss
@@ -29,12 +29,6 @@
 
     <% include Footer %>
 
-	<!-- Scripts -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="{$ThemeDir}/js/vendor/jquery-1.8.3.min.js">\x3C/script>')</script>
-	<script src="{$ThemeDir}/js/plugins-ck.js"></script>
-	<script src="{$ThemeDir}/js/main-ck.js"></script>
-	<script type="text/javascript" src="{$BaseHref}division-bar/js/division-bar.js"></script>
 
 	<script>
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
