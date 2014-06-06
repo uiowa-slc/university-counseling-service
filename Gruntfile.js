86aaa33b6faf2cbe8e8c4809031c98790a67d09b
Dustin Quam <quamsta@gmail.com>
2014-06-06 13:46:17 -0500

    updated to new division standard

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,8 +29,14 @@ module.exports = function(grunt) {
 
     concat: {
       js:{
-        src: ['<%=globalConfig.themeDir %>/js/*.js', 'division-project/js/*.js'],
-        dest: '<%=globalConfig.themeDir %>/build/src/main_concat.js'
+        src: ['division-project/bower_components/jquery/jquery.js',
+          'division-project/bower_components/jquery.equalheights/jquery.equalheights.js',
+          'division-project/bower_components/fitvids/jquery.fitvids.js',
+          'division-project/bower_components/flexslider/jquery.flexslider.js',
+          'division-bar/js/division-bar.js',
+          '<%=globalConfig.themeDir %>/js/*.js', 
+          'division-project/js/*.js'],
+        dest: '<%=globalConfig.themeDir %>/build/build-src.js'
       }
     },
 
@@ -43,7 +49,7 @@ module.exports = function(grunt) {
       },
       my_target:{
         files:{
-        '<%=globalConfig.themeDir %>/build/build.js': ['<%=globalConfig.themeDir %>/build/src/main_concat.js'],
+        '<%=globalConfig.themeDir %>/build/build.js': ['<%=globalConfig.themeDir %>/build/build-src.js'],
         }
       }
     },
